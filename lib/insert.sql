615b88fa62c073c821bb9c2ed1e0cb5fb93e087d
Chinedu Arinze <c.s.arinze@gmail.com>
2019-06-04 22:11:11 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO bears (id, name, age, gender, color, tempermant, alive) VALUES ("Mr. Chocolate", 20, "M", "dark brown", "calm", 0);
+INSERT INTO bears (id, name, age, gender, color, tempermant, alive) VALUES ("Rowdy", 12, "M", "dark brown", "rowdy", 0);
+INSERT INTO bears (id, name, age, gender, color, tempermant, alive) VALUES ("Tabitha", 32, "F", "white", "relaxed", 0);
+INSERT INTO bears (id, name, age, gender, color, tempermant, alive) VALUES ("St. Brown", 12, "M", "brown", "pissed", 0);
+INSERT INTO bears (id, name, age, gender, color, tempermant, alive) VALUES ("Mr. Chocolate", 20, "M", "black", "calm", 0);
+INSERT INTO bears (id, name, age, gender, color, tempermant, alive) VALUES ("Melissa", 3, "F", "dark brown", "anxious", 0);
+INSERT INTO bears (id, name, age, gender, color, tempermant, alive) VALUES ("Wendy", 13, "F", "brown", "Sweet", 0);
+INSERT INTO bears (id, name, age, gender, color, tempermant, alive) VALUES (NULL, 15, "M", "white", "dangerous", 1);
+Mr. Chocolate
+Rowdy
+Tabitha
+Sergeant Brown
+Melissa
+Grinch
+Wendy
