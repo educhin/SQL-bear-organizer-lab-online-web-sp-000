b3f75b1c426fd7636c9f9baedca0b805f150bcbd
Chinedu Arinze <c.s.arinze@gmail.com>
2019-06-04 22:15:36 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -6,10 +6,4 @@ INSERT INTO bears (name, age, gender, color, temperamant, alive) VALUES ("Meliss
 INSERT INTO bears (name, age, gender, color, temperamant, alive) VALUES ("Grinch", 14, "M", "black", "Sweet", 0);
 INSERT INTO bears (name, age, gender, color, temperamant, alive) VALUES ("Wendy", 13, "F", "brown", "Sweet", 0);
 INSERT INTO bears (age, gender, color, temperamant, alive) VALUES (15, "M", "white", "dangerous", 1);
-Mr. Chocolate
-Rowdy
-Tabitha
-Sergeant Brown
-Melissa
-Grinch
-Wendy
+
