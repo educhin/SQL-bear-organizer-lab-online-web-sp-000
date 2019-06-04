b1b1948048cded6bc879bcd2cdd490c951ac2132
Chinedu Arinze <c.s.arinze@gmail.com>
2019-06-04 22:22:00 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,4 +1,4 @@
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 20, "M", "dark brown", "calm", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 20, "M", "dark brown", "calm", 0);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 12, "M", "dark brown", "rowdy", 0);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 32, "F", "white", "relaxed", 0);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 12, "M", "brown", "pissed", 0);
